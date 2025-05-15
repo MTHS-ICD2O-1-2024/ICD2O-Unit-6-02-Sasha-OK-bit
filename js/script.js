481f8b86f1673cf16540525745c31f57262d7736
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-05-15 17:28:22 +0000

    Done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Sasha Vorontsov
+// Created on: April 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+  if (navigator.serviceWorker) {
++   navigator.serviceWorker.register("/ICD2O-Unit-6-02-Sasha-OK-bit/sw.js",
+  {
+  scope: "/ICD2O-Unit-6-02-Sasha-OK-bit/",
+    })
+}
+
+function calculaterandomnumber () {
+
+if( localStorage.savedCookieNumber ) (
+  localStorage.savedCookie = Number(localStorage.savedCookieNumber) +1
+);
+else{
+  localStorage.savedCookieNumber = 1;
+} 
+document.getElementById("answer")
+<p>"Total Hits :" + localStorage.savedCookieNumber </p> 
+}
